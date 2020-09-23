97759f1f855ca90e1c38bb1c176e26106933028b
Rulo Kobashikawa <akobashikawa@gmail.com>
2020-09-22 23:23:10 -0500

    Muestra arcana mayor
    Mejora carga de app

---
+++ js/components/app.js
@@ -0,0 +1,40 @@
+import Deck from './deck-01.js';
+
+const Component = {
+  name: 'app',
+
+  data() {
+    return {
+      cards: Deck,
+    };
+  },
+
+  template: `<div>
+    <b-container>
+      <h1>Tarot Reader</h1>
+
+      <h2>Cartas</h2>
+
+      <b-row>
+        <b-col md="3" v-for="card of cards">
+          <b-card
+            :title="card.order + ': ' + card.title"
+            :img-src="card.imageSrc"
+            :img-alt="card.title"
+            img-top
+            class="shadow mb-2"
+          >
+            <b-card-text>{{ card.description }}</b-card-text>
+          </b-card>
+        </b-col>
+      </b-row>
+
+    </b-container>
+
+    <footer class="text-center mt-3">
+      <small>Tarot Reader - 2020 - Rulo Kobashikawa</small>
+    </footer>
+  </div>`
+};
+
+export default Component;
--- js/main.js
+++ js/main.js
@@ -1,31 +1,5 @@
+import App from './components/app.js';
+
 var app = new Vue({
-  el: '#app',
-
-  data() {
-    return {
-      cards: [],
-    };
-  },
-
-  template: `<div>
-    <b-container>
-      <h1>Tarot Reader</h1>
-
-      <h2>Cartas</h2>
-
-      <b-row>
-        <b-col sm="4">
-          <b-card
-            title="El Loco"
-            img-src="img/maj00.jpg"
-            img-alt="El Loco"
-            img-top
-          >
-            <b-card-text>Una persona inocente y buena al inicio de un viaje.</b-card-text>
-          </b-card>
-        </b-col>
-      </b-row>
-
-    </b-container>
-  </div>`
-});
+  render: h => h(App),
+}).$mount('#app');
